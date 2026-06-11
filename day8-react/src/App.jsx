b03bd274a300d29e0377fa94733f88314fdb58cd
Dev Saini <devsaini0110@gmail.com>
2026-06-11 12:10:00 +0530

    Initial portfolio upload

---
+++ day8-react/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+
+  return (
+    <div>
+
+      <h1>Dev Saini</h1>
+
+      <p>
+        B.Tech Computer Science Engineering Student
+      </p>
+
+      <h2>Skills</h2>
+
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+
+    </div>
+  );
+
+}
+
+export default App;
